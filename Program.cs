3d257a296f404054cdaf63c9f6b08be52a8a4771
Gabriella Sampaio <dixxgabriellasam@gmail.com>
2025-04-25 22:19:55 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿Console.Clear();
+
+double a, b, c, delta, x1, x2;
+
+Console.Write("Digite o valor de [a]: ");
+a = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o valor de [b]: ");
+b = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o valor de [c]: ");
+c = Convert.ToDouble(Console.ReadLine());
+
+if(a==0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("valor nulo, não pode");
+}
+
+else
+{
+    delta = Math.Pow(b, 2) - 4 *a *c;
+    
+    if(delta < 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("não tem raizes reais, não pode");
+    }
+
+    else
+    {
+        x1 = (-b + Math.Sqrt(delta)) / (2 *a);
+        x2 = (-b - Math.Sqrt(delta)) / (2 *a);
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(@$"As raizes da equação são: X' = {x1:n1} e X"" = {x2:n1}.");
+    }
+}
+Console.ResetColor();
